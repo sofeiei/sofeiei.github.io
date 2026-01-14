18d7531bfe7cac32fe1ad8ab37f02d952c592389
nr <sofphuket.psu@gmail.com>
2026-01-14 09:12:37 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const mainMenu = document.querySelector( '.main-head' );
-window.addiEventListener( 'scroll', ()=> {
-    if( this.scrollY > 100 ){
-        mainMenu.classList.add( 'slidedown' );
-    }else{
-        mainMenu.classList.remove( 'slidedown' );
-    }
-} );
